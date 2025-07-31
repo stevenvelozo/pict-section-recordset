3e1d3c3b02a85d1d0868e1bd94846f6b69bb8c57
Alex Decker <alex.decker@headlight.com>
2025-07-31 13:40:15 -0700

    Make control hashes more unique.

--- source/views/filters/RecordSet-Filter-Base-Range.js
+++ source/views/filters/RecordSet-Filter-Base-Range.js
@@ -20,7 +20,7 @@ class ViewRecordSetSUBSETFilterBaseRange extends ViewRecordSetSUBSETFilterBase
 
 		pRecord.StartClauseDescriptor =
 		{
-			Hash: this.fable.DataFormat.sanitizeObjectKey(this.constructor.name),
+			Hash: this.fable.DataFormat.sanitizeObjectKey(`${pRecord.StartClauseAddress}_${this.constructor.name}`),
 			Address: pRecord.StartClauseAddress,
 			//TODO: figure out a nice pattern for extracting a name for the field from the filter - and allow the filter author to provide the label here
 			Name: pRecord.MinimumLabel || `Minimum ${pRecord.ExternalFilterByColumn || pRecord.ExternalFilterByColumns?.[0] || pRecord.FilterByColumn || pRecord.FilterByColumns?.[0] || 'Value'}`,
@@ -29,7 +29,7 @@ class ViewRecordSetSUBSETFilterBaseRange extends ViewRecordSetSUBSETFilterBase
 
 		pRecord.EndClauseDescriptor =
 		{
-			Hash: this.fable.DataFormat.sanitizeObjectKey(this.constructor.name),
+			Hash: this.fable.DataFormat.sanitizeObjectKey(`${pRecord.EndClauseAddress}_${this.constructor.name}`),
 			Address: pRecord.EndClauseAddress,
 			Name: pRecord.MaximumLabel || `Maximum ${pRecord.ExternalFilterByColumn || pRecord.ExternalFilterByColumns?.[0] || pRecord.FilterByColumn || pRecord.FilterByColumns?.[0] || 'Value'}`,
 			DataType: 'String',
